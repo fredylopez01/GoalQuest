f7f00eb02f0318a078ed59838484f0f990c5c2b4
fredylopez01 <lopezdazafredy@gmail.com>
2026-04-04 21:52:36 -0500

    feat: Create the prisma service and run the project without any mistake

--- task-service/src/app.module.ts
+++ task-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { GoalsModule } from './goals/goals.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),PrismaModule, GoalsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- task-service/src/main.ts
+++ task-service/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('GoalQuest - Task Service')
+    .setDescription('Servicio de metas y tareas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Task Service corriendo en http://localhost:${port}`);
+  console.log(`Swagger en http://localhost:${port}/api/docs`);
 }
 bootstrap();
